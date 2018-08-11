5400ae7406e8881c381942ad1957730b4c47b459
sawphaung <sawshanphaung@icloud.com>
2018-08-11 12:38:08 -0500

    Login & SignUp Forms

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,42 +1,39 @@
 import React from 'react';
-import { Container, Row, Col, Input, Button, Fa, Card, CardBody, ModalFooter } from 'mdbreact';
+import { Button, Container, Form, FormGroup, Input } from 'reactstrap';
 import '../styles/login.css';
 
-class LogIn extends React.Component {
+export default class LogIn extends React.Component {
     render() {
         return (
-            <Container>
-                <section className="form-elegant">
-                    <Row>
-                        <Col md="5">
-                            <Card>
-                                <CardBody className="mx-4">
-                                    <div className="text-center">
-                                        <h3 className="dark-grey-text mb-5"><strong>Sign in</strong></h3>
-                                    </div>
-                                    <Input label="Your email" group type="email" validate error="wrong" success="right" />
-                                    <Input label="Your password" group type="password" validate containerClass="mb-0" />
-                                    <p className="font-small blue-text d-flex justify-content-end pb-3">Forgot <a href="#" className="blue-text ml-1"> Password?</a></p>
-                                    <div className="text-center mb-3">
-                                        <Button type="button"  rounded className="btn-block z-depth-1a">Sign in</Button>
-                                    </div>
-                                    <p className="font-small dark-grey-text text-right d-flex justify-content-center mb-3 pt-2"> or Sign in with:</p>
-                                    <div className="row my-3 d-flex justify-content-center">
-                                        <Button type="button"  rounded className="mr-md-3 z-depth-1a"><Fa icon="facebook" className="blue-text text-center" /></Button>
-                                        <Button type="button" rounded className="mr-md-3 z-depth-1a"><Fa icon="twitter" className="blue-text" /></Button>
-                                        <Button type="button"  rounded className="z-depth-1a"><Fa icon="google-plus" className="blue-text" /></Button>
-                                    </div>
-                                </CardBody>
-                                <ModalFooter className="mx-5 pt-3 mb-1">
-                                    <p className="font-small grey-text d-flex justify-content-end">Not a member? <a href="#" className="blue-text ml-1"> Sign Up</a></p>
-                                </ModalFooter>
-                            </Card>
-                        </Col>
-                    </Row>
-                </section>
-            </Container>
+            <div>
+                <Container>
+                    <div className="header_box">
+                        <div className="login_box">
+                            <h2 className="text-center heading">Quora Clone</h2>
+                            <h5 className="text-center secondary-heading">A place to share knowledge and better understand the world</h5>
+                            <div className="row">
+                                <div className="col buttons">
+                                    <Button color="danger" size="sm">Continue with Google</Button>
+                                    <Button color="success" size="sm">Continue with Facebook</Button>
+                                    <Button color="link">Sign up</Button>
+                                </div>
+                                <div className="col forms">
+                                    <Form>
+                                        <p>Login</p>
+                                        <FormGroup>
+                                            <Input type="email" name="email" id="exampleEmail" className="rounded-0" placeholder="Email"/>
+                                        </FormGroup>
+                                        <FormGroup>
+                                            <Input type="password" name="password" id="examplePassword" className="rounded-0" placeholder="Password" />
+                                        </FormGroup>
+                                        <Button size="sm">Submit</Button>
+                                    </Form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+            </div>
         );
     }
-};
-
-export default LogIn;
+}
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,53 +1,45 @@
 import React from 'react';
-import { Container, Row, Col, Input, Button, Fa, Card, CardBody, ModalFooter } from 'mdbreact';
+import { Button, Container, Form, FormGroup, Label, Input } from 'reactstrap';
 import '../styles/signup.css';
 
-class SignUp extends React.Component {
+export default class SignUp extends React.Component {
     render() {
         return (
-            <Container>
-                <section className="form-light">
-                    <Row>
-                        <Col md="5">
-                            <Card>
-                                <CardBody className="mx-4">
-                                    <div className="text-center">
-                                        <h3 className="pink-text mb-5"><strong>Sign up</strong></h3>
-                                    </div>
-                                    <Input label="Your email" group type="text" validate />
-                                    <Input label="Your password" group type="password" validate />
-                                    <div className="md-form pb-3">
-                                        <div className="form-check my-4">
-                                            <input className="form-check-input" type="checkbox" id="defaultCheck12" />
-                                            <label htmlFor="defaultCheck12" className="grey-text">Accept the<a href="#" className="blue-text"> Terms and Conditions</a></label>
-                                        </div>
-                                    </div>
-                                    <Row className="d-flex align-items-center mb-4">
-                                        <Col md="6" className="text-center">
-                                            <button type="button" className="btn btn-pink btn-block btn-rounded z-depth-1">Sign up</button>
-                                        </Col>
-                                        <Col md="6">
-                                            <p className="font-small grey-text d-flex justify-content-end">Have an account? <a href="#" className="blue-text ml-1"> Log in</a></p>
-                                        </Col>
-                                    </Row>
-                                </CardBody>
-                                <div className="footer pt-3 mdb-color lighten-3">
-                                    <Row className="d-flex justify-content-center">
-                                        <p className="font-small white-text mb-2 pt-3">or Sign up with:</p>
-                                    </Row>
-                                    <Row className="mt-2 mb-3 d-flex justify-content-center">
-                                        <a className="fa-lg p-2 m-2 fb-ic"><Fa className="fa fa-facebook white-text fa-lg"> </Fa></a>
-                                        <a className="fa-lg p-2 m-2 tw-ic"><Fa className="fa fa-twitter white-text fa-lg"> </Fa></a>
-                                        <a className="fa-lg p-2 m-2 gplus-ic"><Fa className="fa fa-google-plus white-text fa-lg"> </Fa></a>
-                                    </Row>
-                                </div>
-                            </Card>
-                        </Col>
-                    </Row>
-                </section>
-            </Container>
+            <div>
+                <Container>
+                    <div className="center">
+                        <div className="center-box">
+                            <h2 className="font-weight-bold">Sign Up</h2>
+                            <div className="form">
+                                <Form>
+                                    <FormGroup>
+                                        <Label for="exampleName" hidden>Name</Label>
+                                        <Input type="text" name="name" id="exampleName" placeholder="Name" />
+                                    </FormGroup>
+                                    {' '}
+                                    <FormGroup>
+                                        <Label for="exampleEmail" hidden>Email</Label>
+                                        <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
+                                    </FormGroup>
+                                    {' '}
+                                    <FormGroup>
+                                        <Label for="examplePassword" hidden>Password</Label>
+                                        <Input type="password" name="password" id="examplePassword" placeholder="Password" />
+                                    </FormGroup>
+                                    {' '}
+                                    <FormGroup>
+                                        <Label for="examplePassword" hidden>Password</Label>
+                                        <Input type="password" name="password" id="examplePassword" placeholder="Confirm Password" />
+                                    </FormGroup>
+                                    {' '}
+                                    <Button size="sm">Sign Up</Button>
+                                </Form>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </Container>
+            </div>
         );
     }
-};
-
-export default SignUp;
+}
