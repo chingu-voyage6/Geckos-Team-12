7f34f79f1ac4d6ba85953cf3930ff2f800c5800d
Blarz hdz <info.redpaz@gmail.com>
2018-08-12 17:56:25 -0600

    setting routes

--- server/app.js
+++ server/app.js
@@ -24,6 +24,9 @@ var routes = require('./routes/auth/login');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -35,7 +38,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../build"));
+  app.use(express.static(path.resolve(__dirname, '../build')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
